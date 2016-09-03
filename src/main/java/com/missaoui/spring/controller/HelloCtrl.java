11e9e400239726d5cd98909559887e29f20563cf
Mahmoud <mahmoud.missawi@gmail.com>
2016-09-03 16:49:04 +0100

    first commit

---
+++ src/main/java/com/missaoui/spring/controller/HelloCtrl.java
@@ -0,0 +1,17 @@
+package com.missaoui.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HelloCtrl {
+	
+	@GetMapping
+	public String hello(Model model){
+		model.addAttribute("name","Mahmoud Missaoui");
+		return "Welcome";
+	}
+
+}
