ba9dd313dff12513e624d288316d11330710c722
Mahmoud <mahmoud.missawi@gmail.com>
2016-09-03 17:00:41 +0100

    Second commit

--- src/main/java/com/missaoui/spring/config/AppConfig.java
+++ src/main/java/com/missaoui/spring/config/AppConfig.java
@@ -11,6 +11,8 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 
+
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.missaoui.spring")
